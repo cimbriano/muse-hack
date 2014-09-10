f063d85344f76f38b34ff25eaf28b9ea89eec66d
William Wnekowicz <william.wnekowicz@gmail.com>
2014-09-10 11:24:40 -0700

    Adds full code from the hackathon, ugly and all

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
+var io = require('socket.io-client');
 var dgram = require("dgram");
 var osc = require('osc-min');
 
 var server = dgram.createSocket("udp4");
+// var socket = io('http://104.131.61.142:8080');
+var socket = io('http://localhost:8080');
 
-server.on("error", function (err) {
-  console.log("server error:\n" + err.stack);
-  server.close();
-});
 
 var oscObj;
+
+var message_counter = 0;
 
 server.on("message", function(msg, rinfo) {
   var error;
   try {
     oscObj = osc.fromBuffer(msg);
+    socket.emit('oscdata', oscObj);
+
+    message_counter += 1;
+
+		if(message_counter % 2000 == 0) {
+			console.log('Recieved 2k messages ' + message_counter / 2000);
+		}
 
 
   } catch (_error) {
     error = _error;
-    return console.log("invalid OSC packet");
+    socket.emit('err', "invalid OSC packet");
   }
 });
 
@@ -27,6 +35,7 @@ server.on("listening", function () {
   console.log("server listening " +
       address.address + ":" + address.port);
 });
+
 
 server.bind(5001);
 // server listening 0.0.0.0:5001
