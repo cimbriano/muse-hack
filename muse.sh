4048c86cd0f46a0632f961a75769c5afc2245fd9
William Wnekowicz <william.wnekowicz@gmail.com>
2014-11-22 12:32:20 -0800

    Adds muse start script

---
+++ muse.sh
@@ -0,0 +1,1 @@
+muse-io --preset 14 --device Muse --dsp --osc-timestamp --osc osc.udp://localhost:5001
--- server.js
+++ server.js
@@ -3,25 +3,31 @@ var dgram = require("dgram");
 var osc = require('osc-min');
 
 var server = dgram.createSocket("udp4");
-// var socket = io('http://104.131.61.142:8080');
-var socket = io('http://localhost:8080');
+var socket = io('http://104.131.10.60:8080');
+// var socket = io('http://localhost:8080');
 
 
 var oscObj;
 
 var message_counter = 0;
+var batch = [];
 
 server.on("message", function(msg, rinfo) {
   var error;
   try {
     oscObj = osc.fromBuffer(msg);
     oscObj['timeInMs'] = Date.now();
-    socket.emit('oscdata', oscObj);
-    console.log(oscObj);
+
+    batch.push(oscObj);
+    // socket.emit('oscdata', oscObj);
+
     message_counter += 1;
 
 		if(message_counter % 2000 == 0) {
-			console.log('Recieved 2k messages ' + message_counter / 2000);
+      console.log('Recieved 2k messages ' + message_counter / 2000);
+      socket.emit('oscbatch', batch);
+      console.log(batch);
+      batch = [];
 		}
 
 
